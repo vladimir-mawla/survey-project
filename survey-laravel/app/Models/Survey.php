915ff666f8cf37f0a91c83a24ad4898af9144d42
vladimir <vladimirmawla2@gmail.com>
2022-06-17 00:00:35 +0300

    Creating relationship between surveys and quesntions tables

--- survey-laravel/app/Models/Survey.php
+++ survey-laravel/app/Models/Survey.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Survey extends Model
 {
     use HasFactory;
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
 }
