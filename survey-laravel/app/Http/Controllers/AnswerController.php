fd7800a431586c523cd7a1f7acc54520c0d30ebe
vladimir <vladimirmawla2@gmail.com>
2022-06-17 01:03:48 +0300

    Creating add answer api

--- survey-laravel/app/Http/Controllers/AnswerController.php
+++ survey-laravel/app/Http/Controllers/AnswerController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class AnswerController extends Controller
 {
-    //
+    public function addAnswer(Request $request){
+
+        $answer = new Answer;
+        $answer->answer = $request->answer;
+        $answer->question_id = $request->question_id;
+        $answer->user_id = $request->user_id;
+        $answer->save();
+
+        return response()->json([
+            "status" => "Success",
+        ], 200);
+    }
 }
