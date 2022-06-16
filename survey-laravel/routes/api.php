1ff118c8d09d2928c8d88349fc14f90fe441502d
vladimir <vladimirmawla2@gmail.com>
2022-06-17 01:40:46 +0300

    Adding answers routes

--- survey-laravel/routes/api.php
+++ survey-laravel/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\SurveyController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\AnswerController;
 
 
 /*
@@ -45,4 +46,9 @@ Route::group(['prefix' => 'questions'], function(){
     Route::post('/addquestion', [QuestionController::class, 'addQuestion']);
     Route::post('/getquestionsofsurveys', [QuestionController::class, 'getQuestionsOfSurveys']);
     Route::post('/deletequestion', [QuestionController::class, 'deleteQuestion']);
+});
+
+Route::group(['prefix' => 'answers'], function(){
+    Route::post('/addanswer', [AnswerController::class, 'addAnswer']);
+    Route::post('/getanswerofquestion', [AnswerController::class, 'getAnswerOfQuestion']);
 });
