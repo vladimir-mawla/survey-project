ccb5191e4c2108894d8fa6f30a4898de591a193e
vladimir <vladimirmawla2@gmail.com>
2022-06-17 00:03:05 +0300

    Creating relationship between answers and quesntions tables

--- survey-laravel/app/Models/Question.php
+++ survey-laravel/app/Models/Question.php
@@ -12,4 +12,9 @@ class Question extends Model
     {
         return $this->hasMany(QuestionOption::class);
     }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
 }
