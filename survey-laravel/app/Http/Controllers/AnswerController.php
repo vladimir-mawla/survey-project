2448ce99242e8c636e98ae5e59fe0f35308bc214
vladimir <vladimirmawla2@gmail.com>
2022-06-17 01:05:29 +0300

    Creating get answer of question api

--- survey-laravel/app/Http/Controllers/AnswerController.php
+++ survey-laravel/app/Http/Controllers/AnswerController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 class AnswerController extends Controller
 {
     public function addAnswer(Request $request){
-
         $answer = new Answer;
         $answer->answer = $request->answer;
         $answer->question_id = $request->question_id;
@@ -16,6 +15,14 @@ class AnswerController extends Controller
 
         return response()->json([
             "status" => "Success",
+        ], 200);
+    }
+    public function getAnswerOfQuestion(Request $request){
+        $question_id = $request->question_id;
+        $answer = Answer::where('question_id', $question_id)->get();
+        return response()->json([
+            "status" => "Success",
+            "item" => $answer,
         ], 200);
     }
 }
