17e78285c0e80347d1bdc24836a0580eafd7da71
vladimir <vladimirmawla2@gmail.com>
2022-06-17 00:52:02 +0300

    Creating add question api

--- survey-laravel/app/Http/Controllers/QuestionController.php
+++ survey-laravel/app/Http/Controllers/QuestionController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
-    //
+    public function addQuestion(Request $request){
+
+        $question = new Question;
+        $question->question_type_id = $request->question_type_id;
+        $question->question_option_id = $request->question_option_id;
+        $question->content = $request->content;
+        $question->survey_id = $request->survey_id;
+        $question->save();
+
+        return response()->json([
+            "status" => "Success",
+        ], 200);
+    }
 }
