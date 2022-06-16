ead9e5bfe3dd536eb841f1ff29211e58d58bb4a6
vladimir <vladimirmawla2@gmail.com>
2022-06-17 01:11:18 +0300

    Creating add new question option api

--- survey-laravel/app/Http/Controllers/QuestionOptionController.php
+++ survey-laravel/app/Http/Controllers/QuestionOptionController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class QuestionOptionController extends Controller
 {
-    //
+    public function addQuestionOption(Request $request){
+
+        $question_option = new QuestionOption;
+        $question_option->question_id = $request->question_id;
+        $question_option->save();
+
+        return response()->json([
+            "status" => "Success",
+        ], 200);
+    }
 }
