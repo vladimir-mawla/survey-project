0b0664895df7a7dd26140e987bf7f803b7ed5bf8
vladimir <vladimirmawla2@gmail.com>
2022-06-17 01:09:50 +0300

    Creating add new question type api

--- survey-laravel/app/Http/Controllers/QuestionTypeController.php
+++ survey-laravel/app/Http/Controllers/QuestionTypeController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class QuestionTypeController extends Controller
 {
-    //
+    public function addQuestionType(Request $request){
+
+        $question_type = new QuestionType;
+        $question_type->question_id = $request->question_id;
+        $question_type->save();
+
+        return response()->json([
+            "status" => "Success",
+        ], 200);
+    }
 }
