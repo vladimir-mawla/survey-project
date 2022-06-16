e0ee36182db0e2285ec571b7a32d38eb2ae9d918
vladimir <vladimirmawla2@gmail.com>
2022-06-17 00:36:46 +0300

    Creating add survey api

--- survey-laravel/app/Http/Controllers/SurveyController.php
+++ survey-laravel/app/Http/Controllers/SurveyController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class SurveyController extends Controller
 {
-    //
+    public function addSurvey(Request $request){
+
+        $survey = new Survey;
+        $survey->name = $request->name;
+        $survey->save();
+
+        return response()->json([
+            "status" => "Success",
+        ], 200);
+    }
 }
