fda4f0584725a14f3c6997242b7259846eb38e2c
vladimir <vladimirmawla2@gmail.com>
2022-06-16 23:17:24 +0300

    Filling questions table

---
+++ survey-laravel/database/migrations/2022_06_16_201547_add_to_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->integer('survey_id')->after('id');
+            $table->integer('question_type_id')->after('id');
+            $table->integer('question_option_id')->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            //
+        });
+    }
+};
