22af04e1a1b28652fe94e86220738b3b7b08c618
vladimir <vladimirmawla2@gmail.com>
2022-06-17 03:04:47 +0300

    Grouping routes by version

--- survey-laravel/routes/api.php
+++ survey-laravel/routes/api.php
@@ -23,43 +23,44 @@ use App\Http\Controllers\QuestionOptionController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['prefix' => 'v1'], function(){
+    Route::group([
+        'middleware' => 'api',
+        'prefix' => 'auth'
+    ], function ($router) {
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/register', [AuthController::class, 'register']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::post('/refresh', [AuthController::class, 'refresh']);
+        Route::get('/user-profile', [AuthController::class, 'userProfile']);    
+    });
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-], function ($router) {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::get('/user-profile', [AuthController::class, 'userProfile']);    
-});
+    Route::group(['prefix' => 'surveys'], function(){
+        Route::post('/addsurvey', [SurveyController::class, 'addSurvey']);
+        Route::post('/searchsurvey', [SurveyController::class, 'searchSurvey']);
+        Route::post('/getsurveybyid', [SurveyController::class, 'getSurveyById']);
+        Route::get('/getsurveys', [SurveyController::class, 'getSurveys']);
+        Route::post('/deletesurveys', [SurveyController::class, 'deleteSurveys']);
+        Route::post('/getsurveyanswers', [SurveyController::class, 'getSurveyAnswers']);
+    });
 
-Route::group(['prefix' => 'surveys'], function(){
-    Route::post('/addsurvey', [SurveyController::class, 'addSurvey']);
-    Route::post('/searchsurvey', [SurveyController::class, 'searchSurvey']);
-    Route::post('/getsurveybyid', [SurveyController::class, 'getSurveyById']);
-    Route::get('/getsurveys', [SurveyController::class, 'getSurveys']);
-    Route::post('/deletesurveys', [SurveyController::class, 'deleteSurveys']);
-    Route::post('/getsurveyanswers', [SurveyController::class, 'getSurveyAnswers']);
-});
+    Route::group(['prefix' => 'questions'], function(){
+        Route::post('/addquestion', [QuestionController::class, 'addQuestion']);
+        Route::post('/getquestionsofsurveys', [QuestionController::class, 'getQuestionsOfSurveys']);
+        Route::post('/deletequestion', [QuestionController::class, 'deleteQuestion']);
+    });
 
-Route::group(['prefix' => 'questions'], function(){
-    Route::post('/addquestion', [QuestionController::class, 'addQuestion']);
-    Route::post('/getquestionsofsurveys', [QuestionController::class, 'getQuestionsOfSurveys']);
-    Route::post('/deletequestion', [QuestionController::class, 'deleteQuestion']);
-});
+    Route::group(['prefix' => 'answers'], function(){
+        Route::post('/addanswer', [AnswerController::class, 'addAnswer']);
+        Route::post('/getanswerofquestion', [AnswerController::class, 'getAnswerOfQuestion']);
+    });
 
-Route::group(['prefix' => 'answers'], function(){
-    Route::post('/addanswer', [AnswerController::class, 'addAnswer']);
-    Route::post('/getanswerofquestion', [AnswerController::class, 'getAnswerOfQuestion']);
-});
+    Route::group(['prefix' => 'questiontypes'], function(){
+        Route::post('/addquestiontype', [QuestionTypeController::class, 'addQuestionType']);
+    });
 
-Route::group(['prefix' => 'questiontypes'], function(){
-    Route::post('/addquestiontype', [QuestionTypeController::class, 'addQuestionType']);
-});
-
-Route::group(['prefix' => 'questionoptions'], function(){
-    Route::post('/addquestionoption', [QuestionOptionController::class, 'addQuestionOption']);
-    Route::post('/getquestionoptions', [QuestionOptionController::class, 'getQuestionOptions']);
+    Route::group(['prefix' => 'questionoptions'], function(){
+        Route::post('/addquestionoption', [QuestionOptionController::class, 'addQuestionOption']);
+        Route::post('/getquestionoptions', [QuestionOptionController::class, 'getQuestionOptions']);
+    });
 });
