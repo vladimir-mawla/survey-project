686f9bff2c048d754f3faa3b1ad3cf00b4e88168
vladimir <vladimirmawla2@gmail.com>
2022-06-18 04:09:37 +0300

    Creating login component

---
+++ survey-frontend/src/components/Login.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import Button from './Button';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    function login(){
+        console.log('logging in')
+    }
+  return (
+    <div>
+      <div>
+          <input type="email" placeholder="email" id="login_email" />
+          <input type="password" placeholder="password" id="login_password" />
+          <Button text={'Login'} className={'login-btn'} onClick = {() => {
+              login()
+          }}/>
+          <p className="goto-link">
+            <Link to={'/signup'}>Create Account</Link>
+          </p>
+      </div>
+    </div>
+  );
+};
+export default Login;
