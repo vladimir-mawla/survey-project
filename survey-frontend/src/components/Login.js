465be277ab158f4f865cd0e7533980bc0eea8a86
vladimir <vladimirmawla2@gmail.com>
2022-06-18 21:01:40 +0300

    Adding fetch request to login page

--- survey-frontend/src/components/Login.js
+++ survey-frontend/src/components/Login.js
@@ -1,24 +1,58 @@
 import React from "react";
-import ReactDOM from 'react-dom';
-import Button from './Button';
-import { Link } from 'react-router-dom';
+import ReactDOM from "react-dom";
+import Button from "./Button";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    function login(){
-        console.log('logging in')
-    }
+  function login() {
+    const login_email = document.getElementById("login_email");
+    const login_password = document.getElementById("login_password");
+    console.log(login_email.value);
+    fetch("http://127.0.0.1:8000/api/v1/auth/login", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json, text-plain, /",
+        "X-Requested-With": "XMLHttpRequest",
+      },
+
+      method: "post",
+      credentials: "same-origin",
+      body: JSON.stringify({
+        email: login_email.value,
+        password: login_password.value,
+      }),
+    }).then((response) =>
+      response
+        .json()
+        .then((data) => ({
+          data: data,
+          status: response.status,
+        }))
+        .then((res) => console.log(res.data))
+    );
+  }
+
   return (
     <div>
-      <form>
-          <input type="email" placeholder="email" id="login_email" />
-          <input type="password" placeholder="password" id="login_password" autoComplete="on"/>
-          <Button text={'Login'} className={'login-btn'} onClick = {() => {
-              login()
-          }}/>
-          <p className="goto-link">
-            <Link to={'/signup'}>Create Account</Link>
-          </p>
-      </form>
+      <div>
+        <input type="email" placeholder="email" id="login_email" />
+        <input
+          type="password"
+          placeholder="password"
+          id="login_password"
+          autoComplete="on"
+        />
+        <Button
+          text={"Login"}
+          className={"login-btn"}
+          onClick={() => {
+            login();
+          }}
+        />
+        <p className="goto-link">
+          <Link to={"/signup"}>Create Account</Link>
+        </p>
+      </div>
     </div>
   );
 };
--- survey-frontend/src/components/SurveyComponent.js
+++ survey-frontend/src/components/SurveyComponent.js
@@ -6,6 +6,14 @@ const SurveyComponent = () => {
     
     const surveys = ["survey1", "survey2", "survey3", "survey4"]; 
     
+    // fetch("http://127.0.0.1:8000/api/v1/surveys/getsurveys", {
+    //     method: "get",
+    //     credentials: "same-origin",
+    //   })
+    //   console.log("hello")
+    //     .then((response) => {
+    //         console.log("hello")
+    //     })
 
   try {return (
     <div>{surveys.map((survey, index) => ( 
@@ -16,6 +24,12 @@ const SurveyComponent = () => {
     } catch(error) {
         console.log(error)
     }
+
+
+
+
+
+
 };
 
 export default SurveyComponent;
