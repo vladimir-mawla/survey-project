476f5bae2e58a2d5c8aab7944e24ce231837077e
vladimir <vladimirmawla2@gmail.com>
2022-06-18 17:45:28 +0300

    Adding navbar to admin page

--- survey-frontend/src/components/AdminPage.js
+++ survey-frontend/src/components/AdminPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Button from './Button'
+import Button from './Button';
+import Navbar from './Navbar';
 
 const AdminPage = () => {
     return (
         <div>
+            <Navbar />
             test
         </div>
     )
