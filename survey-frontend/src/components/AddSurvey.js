e2e7a59657b43e496bfdc4e295c1a14e3c593212
vladimir <vladimirmawla2@gmail.com>
2022-06-18 17:34:39 +0300

    Filling add survey name component

--- survey-frontend/src/components/AddSurvey.js
+++ survey-frontend/src/components/AddSurvey.js
@@ -1,1 +1,22 @@
 import React from 'react'
+import Button from './Button';
+
+const AddSurveyName = () => {
+    function addSurveyName(){
+        console.log('Signing Up')
+    }
+  return (
+    <div>
+      <form>
+          <input type="text" placeholder="Survey Name" id="survey_name" />
+          <Button text={'Submit'} className={'survey-name-btn'} onClick = {() => {
+              addSurveyName()
+          }}/>
+          <p className="goto-link">
+          
+          </p>
+      </form>
+    </div>
+  );
+};
+export default AddSurveyName;
