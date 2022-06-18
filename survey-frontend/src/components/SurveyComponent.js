080f40e209c8803b1c8e66a540e277ddf10a97b5
vladimir <vladimirmawla2@gmail.com>
2022-06-18 20:12:21 +0300

    Correcting admin state in survey component

--- survey-frontend/src/components/SurveyComponent.js
+++ survey-frontend/src/components/SurveyComponent.js
@@ -1,16 +1,21 @@
 import React from "react";
-
+import { useState } from "react";
 
 const SurveyComponent = () => {
+    var admin = true;
+    
     const surveys = ["survey1", "survey2", "survey3", "survey4"]; 
     
-    const pablo = surveys.map((survey, index) => ( 
-    <p key={index}> hello {survey}</p>
-    ));
-    
-  return (
-    <div>{pablo}</div>
+
+  try {return (
+    <div>{surveys.map((survey, index) => ( 
+
+        <p id={index}> hello {survey} {admin ? 'x' : ''}</p>
+        ))}</div>
   );
+    } catch(error) {
+        console.log(error)
+    }
 };
 
 export default SurveyComponent;
