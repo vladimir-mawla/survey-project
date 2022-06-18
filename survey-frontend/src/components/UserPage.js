cbd34e070412d0d3d9be6ee7ca2fe07326ea1cc2
vladimir <vladimirmawla2@gmail.com>
2022-06-18 17:47:53 +0300

    Filling user page

---
+++ survey-frontend/src/components/UserPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const UserPage = () => {
+    return (
+        <div>
+            <Navbar />
+            test user
+        </div>
+    )
+}
+
+export default UserPage;
