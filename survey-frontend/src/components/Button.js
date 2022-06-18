a51d8e95fe1cd0f0ec6d394238023bc33e9ea2b4
vladimir <vladimirmawla2@gmail.com>
2022-06-18 03:55:25 +0300

    Creating button component

---
+++ survey-frontend/src/components/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Button = ({ className, text, onClick }) =>{
+    return (
+        <button 
+        className={className}
+        onClick={onClick}
+        >
+            {text}
+        </button>
+    )
+}
+
+export default Button;
