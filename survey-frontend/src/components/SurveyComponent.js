e8268f500ee05e69ed835605de97a4328fc64174
vladimir <vladimirmawla2@gmail.com>
2022-06-19 03:37:43 +0300

    Adding axios to survey component file and listing results

--- survey-frontend/src/components/SurveyComponent.js
+++ survey-frontend/src/components/SurveyComponent.js
@@ -1,35 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
-
+const axios = require('axios').default;
 const SurveyComponent = () => {
-    var admin = true;
-    
-    const surveys = ["survey1", "survey2", "survey3", "survey4"]; 
-    
-    // fetch("http://127.0.0.1:8000/api/v1/surveys/getsurveys", {
-    //     method: "get",
-    //     credentials: "same-origin",
-    //   })
-    //   console.log("hello")
-    //     .then((response) => {
-    //         console.log("hello")
-    //     })
+  var admin = true;
+  const [surveys, setSurveys]= useState([])
 
-  try {return (
-    <div>{surveys.map((survey, index) => ( 
+//   const surveys = ["survey1", "survey2", "survey3", "survey4"];
+  var cat = document.getElementById("cat_search");
+useEffect(() => {
+  axios
+  .get("http://127.0.0.1:8000/api/v1/surveys/getsurveys")
 
-        <p id={index}> hello {survey} {admin ? 'x' : ''}</p>
-        ))}</div>
-  );
-    } catch(error) {
-        console.log(error)
-    }
+    .then((response) => {
 
-
-
-
-
-
+        const s = response.data["surveys"]
+        setSurveys(s)
+    //   return (
+    //     <div>
+    //       <h1>hello</h1>
+    //       {response.data["surveys"].map((survey) => (
+    //         <div id={survey["id"]}>
+    //           <p>
+    //             {" "}
+    //             hello {survey["name"]} {admin ? "x" : ""}
+    //           </p>
+    //         </div>
+    //       ))}
+    //     </div>
+    //   );
+    });
+}, [])
+return (
+    <div>
+        <ul>
+            {surveys.map(survey => (
+                <li id={survey.id} key={survey.id}>{survey.name}</li>
+            ))}
+        </ul>
+    </div>
+)
 };
 
 export default SurveyComponent;
