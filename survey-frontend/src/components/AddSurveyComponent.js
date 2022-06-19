ac149d82cb4b03b3bc73361e1301dd359fbba62c
vladimir <vladimirmawla2@gmail.com>
2022-06-19 17:18:35 +0300

    Adding axios to add survey component

--- survey-frontend/src/components/AddSurveyComponent.js
+++ survey-frontend/src/components/AddSurveyComponent.js
@@ -1,6 +1,17 @@
 import React from "react";
 
 const AddSurveyComponent = () => {
-
+    const answer = document.getElementById('answer')
+    const name = answer.value
+    useEffect(() => {
+        axios
+          .post("http://127.0.0.1:8000/api/v1/surveys/addsurvey", {
+            name,
+          })
+    
+          .then((response) => {
+            localStorage.setItem('survey_id', response.data["survey"]["id"]);
+          });
+      }, []);
 }
 export default AddSurveyComponent;
