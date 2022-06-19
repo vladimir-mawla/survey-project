89745794a0d3b16b091b7a03321a972fc199be41
vladimir <vladimirmawla2@gmail.com>
2022-06-19 04:27:56 +0300

    Adding fetch request to question component file

--- survey-frontend/src/components/QuestionComponent.js
+++ survey-frontend/src/components/QuestionComponent.js
@@ -1,1 +1,32 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const QuestionComponent = () => {
+    const [questions, setQuestions]= useState([])
+
+    const user_type = localStorage.getItem('user_type');
+    const survey_id = 1
+    useEffect(() => {
+      axios
+      .post("http://127.0.0.1:8000/api/v1/questions/getquestionsofsurveys", {survey_id} )
+    
+        .then((response) => {
+            const s = response.data["questions"]
+            setQuestions(s)
+        });
+    }, [])
+    
+    return (
+        <div>
+            <ul>
+                {questions.map(question => (
+                    <li id={question.id} key={question.id} >
+                        {question.content}</li>
+                ))}
+            </ul>
+        </div>
+    )
+    };
+
+export default QuestionComponent;
