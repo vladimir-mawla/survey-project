8e17c0e91e206d966d294f8f56cb5f5faded9684
vladimir <vladimirmawla2@gmail.com>
2022-06-19 05:08:55 +0300

    Linking answers api to question component

--- survey-frontend/src/components/Login.js
+++ survey-frontend/src/components/Login.js
@@ -40,6 +40,8 @@ const Login = () => {
 
           localStorage.setItem('access_token', res.data["access_token"]);
           localStorage.setItem('user_type', res.data["user"]["user_type"]);
+          localStorage.getItem("user_id", res.data["user"]["user_id"]);
+
           if(res.data["user"]["user_type"] == 1){
             navigate('/adminpage')
           }else{navigate('/userpage')}
--- survey-frontend/src/components/QuestionComponent.js
+++ survey-frontend/src/components/QuestionComponent.js
@@ -1,32 +1,48 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const QuestionComponent = () => {
-    const [questions, setQuestions]= useState([])
+  const [questions, setQuestions] = useState([]);
 
-    const user_type = localStorage.getItem('user_type');
-    const survey_id = localStorage.getItem('survey_id')
-    useEffect(() => {
-      axios
-      .post("http://127.0.0.1:8000/api/v1/questions/getquestionsofsurveys", {survey_id} )
-    
-        .then((response) => {
-            const s = response.data["question"]
-            setQuestions(s)
-        });
-    }, [])
-    
-    return (
-        <div>
-            <ul>
-                {questions.map(question => (
-                    <li id={question.id} key={question.id} >
-                        {question.content}</li>
-                ))}
-            </ul>
-        </div>
-    )
-    };
+  const user_type = localStorage.getItem("user_type");
+  const survey_id = localStorage.getItem("survey_id");
+  const user_id = localStorage.getItem("user_id");
+
+  useEffect(() => {
+    axios
+      .post("http://127.0.0.1:8000/api/v1/questions/getquestionsofsurveys", {
+        survey_id,
+      })
+
+      .then((response) => {
+        const s = response.data["question"];
+        setQuestions(s);
+      });
+  }, []);
+  useEffect(() => {
+    axios
+      .post("http://127.0.0.1:8000/api/v1/answers/addanswer", { user_id, survey_id, question_id, answer })
+
+      .then((response) => {
+        const a = response.data["answer"];
+      });
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {questions.map((question) => (
+          <div>
+            <li id={question.id} key={question.id}>
+              {question.content}
+            </li>
+            
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default QuestionComponent;
