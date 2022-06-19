6aafd2f59dbde8a1b4e6a107d55a4aecae7f3fea
vladimir <vladimirmawla2@gmail.com>
2022-06-19 07:04:52 +0300

    Adding conditions for question types

--- survey-frontend/src/components/QuestionComponent.js
+++ survey-frontend/src/components/QuestionComponent.js
@@ -4,11 +4,12 @@ import axios from "axios";
 
 const QuestionComponent = () => {
   const [questions, setQuestions] = useState([]);
+  const [questiontype, setQuestionType] = useState([]);
 
   const user_type = localStorage.getItem("user_type");
   const survey_id = localStorage.getItem("survey_id");
   const user_id = localStorage.getItem("user_id");
-  let answer = document.getElementById('answer')
+  let answer = document.getElementById("answer");
   useEffect(() => {
     axios
       .post("http://127.0.0.1:8000/api/v1/questions/getquestionsofsurveys", {
@@ -16,33 +17,88 @@ const QuestionComponent = () => {
       })
 
       .then((response) => {
+        console.log(response.data["question"][0]["question_type_id"]);
         const s = response.data["question"];
         setQuestions(s);
+        const q = response.data["question"][0]["question_type_id"];
+        setQuestionType(q);
       });
   }, []);
   useEffect(() => {
     axios
-      .post("http://127.0.0.1:8000/api/v1/answers/addanswer", { user_id, survey_id, answer })
+      .post("http://127.0.0.1:8000/api/v1/answers/addanswer", {
+        user_id,
+        survey_id,
+        answer,
+      })
 
       .then((response) => {
         const a = response.data["answer"];
       });
   }, []);
-
-  return (
-    <div>
-      <ul>
-        {questions.map((question) => (
-          <div>
-            <li id={question.id} key={question.id}>
-              {question.content}
-            </li>
-            {/* <input id='answer'></input> */}
-          </div>
-        ))}
-      </ul>
-    </div>
-  );
+  if (questiontype === 1) {
+    return (
+      <div>
+        <ul>
+          {questions.map((question) => (
+            <div>
+              <li id={question.id} key={question.id}>
+                {question.content}
+              </li>
+              <input id="answer"></input>
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+  } else if (questiontype === 2) {
+    return (
+      <div>
+        <ul>
+          {questions.map((question) => (
+            <div>
+              <li id={question.id} key={question.id}>
+                {question.content}
+              </li>
+            </div>
+          ))}
+          {questionoptions.map((option) => (
+            <div>
+              <input
+                id="answer"
+                type="radio"
+                value={question_option.id}
+                name={question_option.name}
+              />
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+  } else if (questiontype === 3) {
+    return (
+      <div>
+        <ul>
+          {questions.map((question) => (
+            <div>
+              <li id={question.id} key={question.id}>
+                {question.content}
+              </li>
+            </div>
+          ))}
+          {questionoptions.map((option) => (
+            <div>
+              <checkbox
+                id="answer"
+                type="radio"
+                value={question_option.id}
+                name={question_option.name}
+              />
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 };
-
 export default QuestionComponent;
