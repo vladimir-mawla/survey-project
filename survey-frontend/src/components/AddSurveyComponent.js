9ef79e9aaf4c527e16fe55d390fb76edca4e9e44
vladimir <vladimirmawla2@gmail.com>
2022-06-19 17:27:53 +0300

    Adding return to add survey component

--- survey-frontend/src/components/AddSurveyComponent.js
+++ survey-frontend/src/components/AddSurveyComponent.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 const AddSurveyComponent = () => {
+    function submit(){
+        
+    
     const answer = document.getElementById('answer')
     const name = answer.value
     useEffect(() => {
@@ -13,5 +16,18 @@ const AddSurveyComponent = () => {
             localStorage.setItem('survey_id', response.data["survey"]["id"]);
           });
       }, []);
+    }
+      return (
+        <div>
+            <input type="text" placeholder="Survey Name" id="answer" />
+            <Button
+              text={"Submit"}
+              className={"submit-answer"}
+              onClick={() => {
+                submit();
+              }}
+            />
+          </div>
+      );
 }
 export default AddSurveyComponent;
