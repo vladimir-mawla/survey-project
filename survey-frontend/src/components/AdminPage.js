f42a3494859b73279102bb0076ed3028e04382b2
vladimir <vladimirmawla2@gmail.com>
2022-06-19 03:45:33 +0300

    Adding survey component in admin page

--- survey-frontend/src/components/AdminPage.js
+++ survey-frontend/src/components/AdminPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Button from './Button';
 import Navbar from './Navbar';
+import SurveyComponent from './SurveyComponent';
 
 const AdminPage = () => {
     return (
         <div>
             <Navbar />
             test
+            <SurveyComponent />
         </div>
     )
 }
