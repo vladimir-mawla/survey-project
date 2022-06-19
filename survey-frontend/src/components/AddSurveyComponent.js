e6c4944d323d8ae5f575176b95a2365c65606393
vladimir <vladimirmawla2@gmail.com>
2022-06-19 17:40:49 +0300

    Removing use effect from add survey component

--- survey-frontend/src/components/AddSurveyComponent.js
+++ survey-frontend/src/components/AddSurveyComponent.js
@@ -1,33 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Button from "./Button";
+import axios from "axios";
 
 const AddSurveyComponent = () => {
-    function submit(){
-        
-    
-    const answer = document.getElementById('answer')
-    const name = answer.value
-    useEffect(() => {
-        axios
-          .post("http://127.0.0.1:8000/api/v1/surveys/addsurvey", {
-            name,
-          })
-    
-          .then((response) => {
-            localStorage.setItem('survey_id', response.data["survey"]["id"]);
-          });
-      }, []);
-    }
-      return (
-        <div>
-            <input type="text" placeholder="Survey Name" id="answer" />
-            <Button
-              text={"Submit"}
-              className={"submit-answer"}
-              onClick={() => {
-                submit();
-              }}
-            />
-          </div>
-      );
-}
+  function submit() {
+    const answer = document.getElementById("answer");
+    const name = answer.value;
+
+    axios
+      .post("http://127.0.0.1:8000/api/v1/surveys/addsurvey", {
+        name,
+      })
+
+      .then((response) => {
+        localStorage.setItem("survey_id", response.data["survey"]["id"]);
+      });
+  }
+  return (
+    <div>
+      <input type="text" placeholder="Survey Name" id="answer" />
+      <Button
+        text={"Submit"}
+        className={"submit-answer"}
+        onClick={() => {
+          submit();
+        }}
+      />
+    </div>
+  );
+};
 export default AddSurveyComponent;
--- survey-frontend/src/components/AdminPage.js
+++ survey-frontend/src/components/AdminPage.js
@@ -4,12 +4,14 @@ import Navbar from './Navbar';
 import SurveyComponent from './SurveyComponent';
 import AddSurveyComponent from './AddSurveyComponent';
 
+
 const AdminPage = () => {
     return (
         <div>
             <Navbar />
             test
             <SurveyComponent />
+            Add Survey
             <AddSurveyComponent />
         </div>
     )
