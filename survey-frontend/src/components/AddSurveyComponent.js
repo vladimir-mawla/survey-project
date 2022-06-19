bd4894117476335237f1d07165cd7f335184db41
vladimir <vladimirmawla2@gmail.com>
2022-06-19 17:58:21 +0300

    Fixing some errors

--- survey-frontend/src/components/AddSurveyComponent.js
+++ survey-frontend/src/components/AddSurveyComponent.js
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useRef } from "react";
 import Button from "./Button";
 import axios from "axios";
+import { Navigate } from "react-router-dom";
 
 const AddSurveyComponent = () => {
+    const input = useRef(null)
+
   function submit() {
     const answer = document.getElementById("answer");
     const name = answer.value;
-
     axios
       .post("http://127.0.0.1:8000/api/v1/surveys/addsurvey", {
         name,
@@ -15,10 +17,12 @@ const AddSurveyComponent = () => {
       .then((response) => {
         localStorage.setItem("survey_id", response.data["survey"]["id"]);
       });
+      input.current.value = ''
+    //   Navigate('./userpage')
   }
   return (
     <div>
-      <input type="text" placeholder="Survey Name" id="answer" />
+      <input ref={input} type="text" placeholder="Survey Name" id="answer" />
       <Button
         text={"Submit"}
         className={"submit-answer"}
