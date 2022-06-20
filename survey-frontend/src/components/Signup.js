cf47c723343cb52e97df4985258dedfbe26d33de
vladimir <vladimirmawla2@gmail.com>
2022-06-20 23:20:39 +0300

    Styling code

--- survey-frontend/src/components/Signup.js
+++ survey-frontend/src/components/Signup.js
@@ -2,19 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Button from "./Button";
 import { Link } from "react-router-dom";
-import { Navigate } from 'react-router-dom';
+import { Navigate } from "react-router-dom";
 import { Component } from "react";
-import {useNavigate} from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
   const navigate = useNavigate();
   function signup() {
-    
     const signup_name = document.getElementById("signup_name");
     const signup_email = document.getElementById("signup_email");
     const signup_password = document.getElementById("signup_password");
-    const signup_password_confirmation = document.getElementById("signup_password_confirmation");
+    const signup_password_confirmation = document.getElementById(
+      "signup_password_confirmation"
+    );
     fetch("http://127.0.0.1:8000/api/v1/auth/register", {
       headers: {
         "Content-Type": "application/json",
@@ -30,33 +30,32 @@ const Signup = () => {
         password: signup_password.value,
         password_confirmation: signup_password_confirmation.value,
       }),
-    }).then((response) =>
-      response
-        .json()
-        .then((data) => ({
+    })
+      .then((response) =>
+        response.json().then((data) => ({
           data: data,
           status: response.status,
         }))
-    )
-        .then((res) => {
-        if(res.data["message"]){
-
-          localStorage.setItem('access_token', res.data["access_token"]);
-          localStorage.setItem('user_type', res.data["user"]["user_type"]);
+      )
+      .then((res) => {
+        if (res.data["message"]) {
+          localStorage.setItem("access_token", res.data["access_token"]);
+          localStorage.setItem("user_type", res.data["user"]["user_type"]);
           localStorage.getItem("user_id", res.data["user"]["user_id"]);
 
-          if(res.data["user"]["user_type"] == 1){
-            navigate('/adminpage')
-          }else{navigate('/userpage')}
-        }else {
-            alert("User not Found")
-            signup_name.value = "";
-            signup_email.value = "";
-            signup_password.value = "";
-            signup_password_confirmation.value = "";
+          if (res.data["user"]["user_type"] == 1) {
+            navigate("/adminpage");
+          } else {
+            navigate("/userpage");
+          }
+        } else {
+          alert("User not Found");
+          signup_name.value = "";
+          signup_email.value = "";
+          signup_password.value = "";
+          signup_password_confirmation.value = "";
         }
-      })
-  
+      });
   }
 
   return (
@@ -64,8 +63,18 @@ const Signup = () => {
       <div className="form">
         <input type="text" placeholder="Name" id="signup_name" />
         <input type="email" placeholder="Email" id="signup_email" />
-        <input type="password" placeholder="Password" id="signup_password" autoComplete="on" />
-        <input type="password" placeholder="Confirm password" id="signup_password_confirmation" autoComplete="on" />
+        <input
+          type="password"
+          placeholder="Password"
+          id="signup_password"
+          autoComplete="on"
+        />
+        <input
+          type="password"
+          placeholder="Confirm password"
+          id="signup_password_confirmation"
+          autoComplete="on"
+        />
         <Button
           text={"Signup"}
           className={"login-btn"}
